a3741034446177eabe001f704d84d6fefb82fd1f
Huy Hoang <hhoang2197@gmail.com>
2021-11-26 18:41:05 -0600

    Added user genres capability

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -9,35 +9,51 @@ const spotifyApi = new SpotifyWebApi({
 
 export default function Dashboard({ code }) {
   const accessToken = useAuth(code);
-  const [tracks, setTracks] = useState([]);
+  const [genres, setGenres] = useState([]);
 
   useEffect(() => {
     if (!accessToken) return;
     spotifyApi.setAccessToken(accessToken);
     spotifyApi.getUserPlaylists().then((res) => {
-      spotifyApi.getPlaylistTracks(res.body.items[0].id).then((tracks) => {
-        setTracks(
-          tracks.body.items.map((track) => {
-            return {
-              uri: track.track.uri,
-              name: track.track.name,
-              artist: track.track.artist,
-              album: track.track.album.name,
-            };
-          })
-        );
-      });
+      spotifyApi
+        .getPlaylistTracks(res.body.items[0].id)
+        .then(async (tracks) => {
+          var tracksArtistsIds = tracks.body.items.map((trackInfo) => {
+            return trackInfo.track.artists.map((artist) => artist.id);
+          });
+
+          // array of unique ids
+          var artistIDs = [...new Set(tracksArtistsIds.flat())];
+
+          // list of all artist genres
+          var artistGenres = [];
+
+          await Promise.all(
+            artistIDs.map(async (artistId) => {
+              const artist = await spotifyApi.getArtist(artistId);
+              artistGenres.push(...artist.body.genres);
+            })
+          );
+
+          var counts = {};
+          artistGenres.forEach((x) => (counts[x] = (counts[x] || 0) + 1));
+          //console.log(counts);
+
+          var countArray = Object.entries(counts);
+          var sorted = countArray.sort((a, b) => b[1] - a[1]);
+          //console.log(sorted);
+          setGenres(sorted);
+        });
     });
-  }, [accessToken, tracks]);
+  }, [accessToken]);
 
   return (
     <div>
-      {tracks.map((track) => (
-        <div key={track.uri}>
-          <ListItemText primary={track.name} />
+      {genres.map((genre, index) => (
+        <div key={index}>
+          <ListItemText primary={`${genre[0]}: ${genre[1]}`} />
         </div>
       ))}
     </div>
-    //<div>{code}</div>
   );
 }
